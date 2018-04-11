5d13996f7fe56f4cf0021ba8dfd3495b2bbb8860
Rafael Shepard <rafael_gulie94@list.ru>
2018-04-12 00:41:02 +0300

    mobile header

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
  $('#toggle-tab').click(function(){
   $('.tablet-menu').toggle(200);
  });
+  $('.cross-box__item2').click(function(){
+    $('.header').toggle(220);
+  });
 });
