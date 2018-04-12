42ef0a153ca4f98cae2a4477701629453ef2f57c
Rafael Shepard <rafael_gulie94@list.ru>
2018-04-13 01:45:13 +0300

    sss

--- scripts/script.js
+++ scripts/script.js
@@ -10,12 +10,16 @@ $(document).ready(function(){
     toggler.toggle('slow');
     $('.menu').toggleClass('menu--none');
   });
+
  const togglerTwo = $('.b-tablet-menu');
   $('.b-header-tab__img').click(function(){
     togglerTwo.toggle('slow')
     togglerTwo.toggleClass('b-tablet-menu--none');
   });
+
   $('.cross-box__item2--burger').click(function(){
     $(this).toggleClass('cross-box__item2');
   });
+
+
 })
