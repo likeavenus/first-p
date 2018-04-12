0818c45e8b1e077071e200b19b9695682927eabe
Rafael Shepard <rafael_gulie94@list.ru>
2018-04-12 18:31:07 +0300

    .hide

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
  $('#toggle-tab').click(function(){
   $('.tablet-menu').toggle(200);
  });
+ $('.header').hide();
   $('.cross-box__item2').click(function(){
-    $('.header').toggle('fast');
+    $('.header').toggle('slow');
   });
+
 });
