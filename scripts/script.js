5431e5f8e67413c28e77aea6bb625711dc0a82df
Rafael Shepard <rafael_gulie94@list.ru>
2018-04-13 15:19:56 +0300

    ПЩПЩ

--- scripts/script.js
+++ scripts/script.js
@@ -28,12 +28,11 @@ const togglerIndex = $('.mobile-promo');
   });
    $('.cross-box__item2--white-burger').click(function(){
     $('.menu').css({'display' : 'flex'});
-    $('.menu').prepend('<div class="cross-box"><a class="cross-box__link" href="https://likeavenus.github.io/first-p/"><span class="cross-box__item1">ice age</span></a><a href="#" class="cross-box__item2"></a></div>');
-    $('.cross-box__item2').css({'margin-right' : '15px'});
-
-     $('.cross-box__item2').click(function(){
-       $('.mobile-promo').toggle('slow');
-       $('.menu').css({'display' : 'none'});
-   });
   });
+ $('.cross-box2__item').click(function(){
+  $('.mobile-promo').toggle('slow');
+  $('.menu').css({'display' : 'none'});
+ });
 });
+    // $('.menu').prepend('<div class="cross-box"><a class="cross-box__link" href="https://likeavenus.github.io/first-p/"><span class="cross-box__item1">ice age</span></a><a href="#" class="cross-box__item2"></a></div>');
+    // $('.cross-box__item2').css({'margin-right' : '15px'});
